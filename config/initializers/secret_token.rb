5cddf369d1dcde78f55d7cb79a0f9eb3335fca89
Matt Kolb <kolb722@gmail.com>
2013-07-29 15:37:01 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chirrup::Application.config.secret_key_base = 'a24a159f389ffeab8de7573568e91b89e78959abe31bc84cd057588014a676de598b7821f4743539bdc3eb6b168d5ce1bfa135806cb3c00b7de595abab652c87'
