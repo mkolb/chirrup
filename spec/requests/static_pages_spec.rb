c407853b7b8ab60080084a714006ab143a808116
Matt Kolb <kolb722@gmail.com>
2013-07-30 13:03:31 -0400

    Added static pages.

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  let(:title) { 'Chirrup | ' }
+  describe "Home page" do
+    it "should have the content 'Chirrup'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Chirrup')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{title}Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{title}Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{title}About")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{title}Contact")
+    end
+  end
+end
