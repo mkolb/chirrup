cfd048f5c4c4b4baa0e69b2faf3b92cb53b4cd50
Matt Kolb <kolb722@gmail.com>
2013-07-31 09:59:46 -0400

    Added bootstrap styling and refactored the routes.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,49 +4,73 @@ describe "Static pages" do
   let(:title) { 'Chirrup | ' }
   describe "Home page" do
     it "should have the content 'Chirrup'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Chirrup')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{title}Home")
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{title}Help")
     end
   end
 
   describe "About page" do
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{title}About")
     end
   end
 
   describe "Contact page" do
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{title}Contact")
+    end
+  end
+
+  describe "Sign In page" do
+    it "should have the content 'Sign In'" do
+      visit signin_path
+      expect(page).to have_content('Sign In')
+    end
+
+    it "should have the title 'Sign In'" do
+      visit signin_path
+      expect(page).to have_title("#{title}Sign In")
+    end
+  end
+
+  describe "Sign Up page" do
+    it "should have the content 'Sign Up'" do
+      visit signup_path
+      expect(page).to have_content('Sign Up')
+    end
+
+    it "should have the title 'Sign Up'" do
+      visit signup_path
+      expect(page).to have_title("#{title}Sign Up")
     end
   end
 end
